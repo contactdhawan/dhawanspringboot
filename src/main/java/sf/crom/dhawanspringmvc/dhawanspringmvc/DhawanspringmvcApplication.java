69ebb5c19046d3d1b4381216ef570b14c75933c3
Kumar <neeraj.o.kumar@capgemini.com>
2017-10-07 22:51:14 -0500

    first commit

---
+++ src/main/java/sf/crom/dhawanspringmvc/dhawanspringmvc/DhawanspringmvcApplication.java
@@ -0,0 +1,12 @@
+package sf.crom.dhawanspringmvc.dhawanspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhawanspringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhawanspringmvcApplication.class, args);
+	}
+}
---
+++ src/main/java/sf/crom/dhawanspringmvc/dhawanspringmvc/controller/IndexController.java
@@ -0,0 +1,12 @@
+package sf.crom.dhawanspringmvc.dhawanspringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
