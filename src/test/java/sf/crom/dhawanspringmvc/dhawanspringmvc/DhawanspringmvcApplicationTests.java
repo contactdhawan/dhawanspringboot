34dfb8811015d0ef64242e56a6032200afbec7af
Neeraj <contactdhawan@gmail.com>
2017-10-22 17:48:20 -0500

    adding sping mvc test

--- src/test/java/sf/crom/dhawanspringmvc/dhawanspringmvc/DhawanspringmvcApplicationTests.java
+++ src/test/java/sf/crom/dhawanspringmvc/dhawanspringmvc/DhawanspringmvcApplicationTests.java
@@ -1,16 +1,34 @@
 package sf.crom.dhawanspringmvc.dhawanspringmvc;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import sf.crom.dhawanspringmvc.dhawanspringmvc.controller.IndexController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DhawanspringmvcApplicationTests {
 
+	MockMvc mockmvc;
+	IndexController indexController;
+
+	@Before
+	public void setup(){
+		indexController = new IndexController();
+		mockmvc = MockMvcBuilders.standaloneSetup(indexController).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		mockmvc.perform(get("/")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("index"));
 	}
 
 }
